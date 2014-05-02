bb9e6a4c598205c07497ed7b0566ba218dce8d4d
Adolfo Builes <builes.adolfo@gmail.com>
2014-05-02 01:50:50 -0300

    Update ember-cli to version from PR #562 using canary.

--- client/Brocfile.js
+++ client/Brocfile.js
@@ -2,38 +2,57 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
+EmberApp.prototype.populateLegacyFiles = function () {
+
+  this.import('jquery/jquery.js');
+  this.import('handlebars/handlebars.js');
+
+  if (this.env === 'production') {
+    this.import('ember-prod/index.js');
+  } else {
+    this.import('ember/index.js');
+  }
+
+  this.import('ember-cli-shims/app-shims.js', {
+    ember: ['default']
+  });
+
+  this.import('ember-resolver/dist/modules/ember-resolver.js', {
+    'ember/resolver': ['default']
+  });
+
+  this.import('ember-load-initializers/ember-load-initializers.js', {
+    'ember/load-initializers': ['default']
+  });
+};
+
+
 var app = new EmberApp({
   name: require('./package.json').name,
 
-  legacyFilesToAppend: [
-    'jquery.js',
-    'handlebars.js',
-    'ember.js',
-    'ic-ajax/dist/named-amd/main.js',
-    'ember-data.js',
-    'app-shims.js',
-    'ember-resolver.js',
-    'ember-load-initializers.js'
-  ],
+  // Use this to instruct the `broccoli-es6-concatenator` to allow
+  // references to the following modules (this would commonly include
+  // any modules exported from any AMD files added to `legacyFilesToAppend`)
+  ignoredModules: [ ],
 
-  // AKA whitelisted modules
-  ignoredModules: [
-    'ember',
-    'ember/resolver',
-    'ember/load-initializers',
-    'ic-ajax'
-  ],
-
-  // hack we can hopefully remove as the addon system improves
-  importWhitelist: {
-    'ember': ['default'],
-    'ember/resolver': ['default'],
-    'ember/load-initializers': ['default'],
-    'ic-ajax': ['request'],
-  },
+  // Use this to notify the import validator of any AMD modules
+  // that you add to your project.
+  importWhitelist: { },
 
   // hack
   getEnvJSON: require('./config/environment')
 });
+
+app.import('ember-data/ember-data.js');
+app.import('ic-ajax/dist/named-amd/main.js', {
+  'ic-ajax': [
+    'default',
+    'defineFixture',
+    'lookupFixture',
+    'raw',
+    'request',
+  ]
+});
+
 
 module.exports = app.toTree();
--- client/config/environment.js
+++ client/config/environment.js
@@ -2,8 +2,7 @@ module.exports = function(environment) {
   var ENV = {
     baseURL: '/',
     FEATURES: {
-      // Here you can enable experimental features on an ember canary build
-      // e.g. 'with-controller': true
+      "query-params-new" : true
     },
 
     APP: {
