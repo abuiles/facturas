556ce82f608e4db0b52a0da898a4dc71825df2cf
Adolfo Builes <builes.adolfo@gmail.com>
2014-07-06 11:08:34 -0400

    Allow anonymous users to get CSRF tokens.

--- app/controllers/api/v1/csrf_controller.rb
+++ app/controllers/api/v1/csrf_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::CsrfController < ApplicationController
+  skip_before_filter :authenticate_user!
   def index
     render json: { request_forgery_protection_token => form_authenticity_token }.to_json
   end
