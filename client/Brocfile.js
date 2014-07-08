e89d7a4cdabddbfeba1812102ea809e98480870e
Adolfo Builes <builes.adolfo@gmail.com>
2014-07-08 11:10:15 -0700

    Fix options for fingerprinting.

--- client/Brocfile.js
+++ client/Brocfile.js
@@ -1,8 +1,19 @@
 /* global require, module */
 
+var sh = require('execSync');
+var shortSHA = sh.exec('git rev-parse HEAD | cut -c1-7').stdout.split('\n')[0];
+
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp({});
+var app = new EmberApp({
+  fingerprint: {
+    prepend: 'https://d29bb5msqib8gy.cloudfront.net/' + shortSHA + '/'
+  },
+  minifyCSS: {
+    enabled: true,
+    options: {}
+  }
+});
 
 app.import('vendor/custom-plugins/_ember-devise-simple-auth.js', {
   exports: {
